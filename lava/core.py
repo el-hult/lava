27b14e407dbdda619a5a4c6165ce93c9b483af06
Ludvig Hult <ludvig.hult@gmail.com>
2019-05-20 09:40:26 +0200

    Added a intercept regressor class with test

--- lava/core.py
+++ lava/core.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 class LavaBase:
     """ Base class for Lava models
@@ -8,15 +9,30 @@ class LavaBase:
     """
 
     def __init__(self):
-        """
-        Create a new object of the lava class
-
-        Args:
-            foo: some arg
-        """
-        self.x = 1
-
+        """ Create a new object of the lava class """
+        self.y_history = None
+        self.u_history = None
 
     def step(self, y, u):
-        """ Dummy implementation of code to come"""
-        return self.x+y+u
+        """ Perform one step of learning
+
+        Args:
+            y (ndarray): observed output
+            u (ndarray): observed input
+        """
+        if self.y_history is None or self.u_history is None:
+            self.u_history = u
+            self.y_history = y
+        else:
+            self.u_history = np.column_stack([self.u_history, u])
+            self.y_history = np.column_stack([self.y_history, y])
+
+
+class InterceptRegressor:
+    """ A class for returning a constant 1, useful for modelling intercepts."""
+    def __init__(self):
+        pass
+
+    # noinspection PyMethodMayBeStatic
+    def get_latent_regressor(self, y_history, u_history):
+        return np.array([1])
--- tests/test_lavaBase.py
+++ tests/test_lavaBase.py
@@ -1,14 +1,21 @@
 import unittest
+import numpy as np
 
 from lava import LavaBase
 
 
-class TestCausalEstimator(unittest.TestCase):
+class TestLavaBase(unittest.TestCase):
 
     def test_init(self):
         lb = LavaBase()
-        self.assertEqual(lb.x,1)
+        self.assertEqual(1,1)
 
-    def test_step(self):
+    def test_step_stacking(self):
         lb = LavaBase()
-        self.assertEqual(lb.step(3,6),3+6+1)
+        y = np.array([1,4,6]).T
+        u = np.array([5,4]).T
+        lb.step(y, u)
+        lb.step(y, u)
+
+        self.assertEqual((2,2), lb.u_history.shape, f"the u history has shape {lb.u_history.shape}")
+        self.assertEqual((3,2), lb.y_history.shape, f"the y history has shape {lb.y_history.shape}")
